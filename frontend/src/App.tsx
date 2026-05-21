ebc11989f84d14ba28053dfc21660a42b363f1c7
张世冬 <shidongzhang@xyz10.com>
2026-05-21 10:06:44 +0800

    fix: remove redundant margin-right from main content, flex handles layout naturally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PlanningListPage from './pages/PlanningListPage'
 import TripDetailPage from './pages/TripDetailPage'
 import { AgentPanel } from './components/AgentPanel'
-import { useTripStore } from './store'
 
 function App() {
-  const isAgentPanelOpen = useTripStore((s) => s.isAgentPanelOpen)
-
   return (
     <BrowserRouter>
       <div className="flex w-screen h-screen overflow-hidden">
-        <div className={`flex-1 min-w-0 transition-all duration-300 ease-in-out ${isAgentPanelOpen ? 'mr-[420px]' : ''}`}>
+        <div className="flex-1 min-w-0">
           <Routes>
             <Route path='/' element={<PlanningListPage />} />
             <Route path='/trip/:id' element={<TripDetailPage />} />
